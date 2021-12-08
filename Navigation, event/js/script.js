012e0ac65f98aea3f2ceed482e7a48797ab5be0e
RSabiRr <92287092+RSabiRr@users.noreply.github.com>
2021-12-08 00:13:27 -0800

    Add files via upload

---
+++ Navigation, event/js/script.js
@@ -0,0 +1,115 @@
+//task 1
+
+// let col=document.querySelector(".col");
+
+// let p=document.createElement("p");
+
+
+// p.id="myText";
+// p.style.fontSize="50px";
+
+// p.textContent="Hello world";
+
+// col.append(p);
+
+// console.log(col);
+
+
+///////////////////////
+
+//task 2
+
+// let col=document.querySelector(".col");
+
+// let input=document.createElement("input");
+
+// input.id="myInput";
+
+// col.append(input);
+
+// console.log(col);
+
+/////////////////////////////
+
+//task 3
+
+// let col=document.querySelector(".col");
+
+// let p=document.createElement("p");
+
+// p.id="myText";
+
+// p.textContent="Hello world";
+
+// col.append(p);
+
+// console.log(col);
+
+//////////////////////////////
+
+//task 4
+
+// let col=document.querySelector(".col");
+// let p=document.createElement("p");
+
+// p.textContent="Lorem Ipsum is simply dumy text of the printing and typesetting industry"
+
+//  //// a
+//  p.setAttribute("style","background:red");
+
+//  //// b
+// p.style.background="blue";
+
+// col.id="myStatement";
+
+// col.append(p);
+
+// console.log(col);
+
+
+////////////////////
+
+
+/// Radius  Volume 
+
+
+
+// function valuee(){
+
+//     var r,val,pi;
+
+//      r=Number(document.getElementsByClassName("radius")[0].value);
+
+//      pi=3.14;
+
+     
+//     val=(4*pi*r*r*r)/3;
+
+//     document.getElementsByClassName("value")[0].value=val;
+
+//     console.log(val);
+
+// }
+
+
+
+////////////////////////////////
+
+//taskss
+
+// let col=document.querySelector(".col");
+// let p=document.createElement("p");
+
+// p.textContent="p tag"
+
+// p.style.background="yellow";
+
+// col.append(p);
+
+/////////////////////
+
+let col=document.querySelector(".col");
+
+
+
+
